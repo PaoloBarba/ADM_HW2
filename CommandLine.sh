fae555828247b0b8e1c6da84b097a6c4aaa8559e
albachiarabellaroba <112660876+albachiarabellaroba@users.noreply.github.com>
2022-10-30 23:11:25 +0100

    Delete CommandLine.sh

--- CommandLine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-posts_id=$(cat instagram_posts.csv| cut --fields=2,8 -s | grep -oE ‘^[[:digit:]]+[[:blank:]]+.{100,}$’ | head -10 | cut -f1)
-while ifs= read -r id; do
-    if ['$id'=-1]; then
-        echo User was not found!
-    else
-        cat instagram_profiles.csv | cut -f 1,3 | grep -w '$id'| cat
-    fi
-done < '$id'
